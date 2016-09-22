a591da33f114b26c67730e5045b06ee9e9e8f55b
laeeq80 <laeeq@kth.se>
2016-09-22 14:04:18 +0200

    PosePipeline and TopNPoses

--- vs/src/main/scala/se/uu/farmbio/vs/PosePipeline.scala
+++ vs/src/main/scala/se/uu/farmbio/vs/PosePipeline.scala
@@ -1,21 +1,62 @@
 package se.uu.farmbio.vs
 
 import scala.io.Source
-import org.apache.spark.rdd.RDD
+
 import org.apache.spark.Logging
+import org.apache.spark.rdd.RDD
+
 import openeye.oedocking.OEDockMethod
-import org.apache.log4j.Logger
 
 trait PoseTransforms {
 
   def collapse(bestN: Int): SBVSPipeline with PoseTransforms
   def sortByScore: SBVSPipeline with PoseTransforms
   def repartition: SBVSPipeline with PoseTransforms
-
+  def getTopPoses(topN: Int): Array[String]
 }
 
-object PosePipeline extends Logging {
-  private[vs] def parseScore(method: Int) = (pose: String) => {
+private[vs] object PosePipeline extends Logging {
+
+  private def parseId(pose: String) = {
+    Source.fromString(pose).getLines.next
+  }
+
+  private[vs] def parseIdAndScore(method: Int)(pose: String) = {
+    var score: Double = Double.MinValue
+    val id: String = parseId(pose)
+    //Sometimes OEChem produce molecules with empty score or malformed molecules
+    //We use try catch block for those exceptions
+    try {
+      val methodString: String = method match {
+        case OEDockMethod.Chemgauss4 => "Chemgauss4"
+        case OEDockMethod.Chemgauss3 => "Chemgauss3"
+        case OEDockMethod.Shapegauss => "Shapegauss"
+        case OEDockMethod.Chemscore  => "Chemscore"
+        case OEDockMethod.Hybrid     => "Hybrid"
+        case OEDockMethod.Hybrid1    => "Hybrid1"
+        case OEDockMethod.Hybrid2    => "Hybrid2"
+        case OEDockMethod.PLP        => "PLP"
+      }
+      var res: String = null
+      val it = SBVSPipeline.CDKInit(pose)
+      if (it.hasNext()) {
+        val mol = it.next
+        res = mol.getProperty(methodString)
+
+      }
+      score = res.toDouble
+    } catch {
+
+      case exec: Exception => logWarning("Setting the score to Double.MinValue." +
+        "It was not possible to parse the score of the following molecule due to \n" + exec +
+        "\n" + exec.getStackTraceString + "\nPose:\n" + pose)
+
+    }
+    (id, score)
+
+  }
+
+  private[vs] def parseScore(method: Int)(pose: String) = {
     var result: Double = Double.MinValue
     //Sometimes OEChem produce molecules with empty score or malformed molecules
     //We use try catch block for those exceptions
@@ -32,7 +73,7 @@ object PosePipeline extends Logging {
       }
       var res: String = null
       val it = SBVSPipeline.CDKInit(pose)
-      while (it.hasNext()) {
+      if (it.hasNext()) {
         val mol = it.next
         res = mol.getProperty(methodString)
       }
@@ -42,12 +83,9 @@ object PosePipeline extends Logging {
       case exec: Exception => logWarning("Setting the score to Double.MinValue." +
         "It was not possible to parse the score of the following molecule due to \n" + exec +
         "\n" + exec.getStackTraceString + "\nPose:\n" + pose)
+
     }
     result
-  }
-
-  private def parseId = (pose: String) => {
-    Source.fromString(pose).getLines.next
   }
 
   private def collapsePoses(bestN: Int, parseScore: String => Double) = (record: (String, Iterable[String])) => {
@@ -59,10 +97,42 @@ object PosePipeline extends Logging {
 private[vs] class PosePipeline(override val rdd: RDD[String], val scoreMethod: Int) extends SBVSPipeline(rdd)
     with PoseTransforms {
 
+  //Need a local copy due to serialization error 
+  // http://spark-summit.org/wp-content/uploads/2013/10/McDonough-spark-tutorial_spark-summit-2013.pptx
   val methodBroadcast = rdd.sparkContext.broadcast(scoreMethod)
 
+  override def getTopPoses(topN: Int) = {
+    val methodBroadcastLocal = methodBroadcast
+    val method = methodBroadcastLocal.value
+    //Parsing id and Score in parallel and collecting data to driver
+    val idAndScore = rdd.map {
+      case (mol) => PosePipeline.parseIdAndScore(method)(mol)
+    }.collect()
+
+    //Finding Distinct top id and score in serial at driver
+    val topMols =
+      idAndScore.foldLeft(Map[String, Double]() withDefaultValue Double.MinValue) {
+        case (m, (id, score)) => m updated (id, score max m(id))
+      }
+      .toSeq
+      .sortBy { case (id, score) => -score }
+      .take(topN).toArray
+
+    //Broadcasting the top id and score and search main rdd
+    //for top molecules in parallel  
+    val topMolsBroadcast = rdd.sparkContext.broadcast(topMols)
+    val topPoses = rdd.filter { mol =>
+      val idAndScore = PosePipeline.parseIdAndScore(method)(mol)
+      topMolsBroadcast.value
+        .map(topHit => topHit == idAndScore)
+        .reduce(_ || _)
+    }
+    topPoses.collect
+  }
+
   override def sortByScore = {
-    val res = rdd.sortBy(PosePipeline.parseScore(methodBroadcast.value), false)
+    val res = rdd.sortBy(PosePipeline
+      .parseScore(methodBroadcast.value), false)
     new PosePipeline(res, scoreMethod)
   }
 
