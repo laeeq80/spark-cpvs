8564e4ab34d6823346f8ef2f95d2e7a50cc85e29
laeeq80 <laeeq@kth.se>
2016-12-14 14:13:17 +0100

    minor

--- vs.examples/src/main/scala/se/uu/farmbio/vs/examples/DockerWithML.scala
+++ vs.examples/src/main/scala/se/uu/farmbio/vs/examples/DockerWithML.scala
@@ -19,14 +19,14 @@ object DockerWithML extends Logging {
     master: String = null,
     conformersFile: String = null,
     topPosesPath: String = null,
+    firstFile: String = null,
+    secondFile: String = null,
     dsInitSize: Int = 100,
     calibrationSize: Int = 100,
     numIterations: Int = 50,
     topN: Int = 30,
     badIn: Int = 1,
-    goodIn: Int = 4,
-    firstFile: String = null,
-    secondFile: String = null)
+    goodIn: Int = 4)
 
   def main(args: Array[String]) {
     val defaultParams = Arglist()
@@ -35,21 +35,6 @@ object DockerWithML extends Logging {
       opt[String]("master")
         .text("spark master")
         .action((x, c) => c.copy(master = x))
-      opt[Int]("dsInitSize")
-        .text("intial Data to be docked (default: 100)")
-        .action((x, c) => c.copy(dsInitSize = x))
-      opt[Int]("calibrationSize")
-        .text("size of calibration Set (default: 100)")
-        .action((x, c) => c.copy(calibrationSize = x))
-      opt[Int]("numIterations")
-        .text("number of iternations for the ML model training (default: 100)")
-        .action((x, c) => c.copy(numIterations = x))
-      opt[Int]("badIn")
-        .text("UpperBound of bad bins")
-        .action((x, c) => c.copy(badIn = x))
-      opt[Int]("goodIn")
-        .text("LowerBound of good bins")
-        .action((x, c) => c.copy(goodIn = x))
       arg[String]("<conformers-file>")
         .required()
         .text("path to input SDF conformers file")
@@ -66,6 +51,21 @@ object DockerWithML extends Logging {
         .required()
         .text("path to input file that you want to check for accuracy")
         .action((x, c) => c.copy(secondFile = x))
+      opt[Int]("dsInitSize")
+        .text("intial Data to be docked (default: 100)")
+        .action((x, c) => c.copy(dsInitSize = x))
+      opt[Int]("calibrationSize")
+        .text("size of calibration Set (default: 100)")
+        .action((x, c) => c.copy(calibrationSize = x))
+      opt[Int]("numIterations")
+        .text("number of iternations for the ML model training (default: 100)")
+        .action((x, c) => c.copy(numIterations = x))
+      opt[Int]("badIn")
+        .text("UpperBound of bad bins")
+        .action((x, c) => c.copy(badIn = x))
+      opt[Int]("goodIn")
+        .text("LowerBound of good bins")
+        .action((x, c) => c.copy(goodIn = x))
       opt[Int]("topN")
         .text("number of top scoring poses to extract (default: 30).")
         .action((x, c) => c.copy(topN = x))
@@ -95,7 +95,6 @@ object DockerWithML extends Logging {
       .readConformerFile(params.conformersFile)
       .getMolecules
       .flatMap { mol => SBVSPipeline.splitSDFmolecules(mol) }
-    //.generateSignatures()
 
     val posesWithSigns = new ConformersWithSignsAndScorePipeline(poses)
       .dockWithML(params.dsInitSize,
@@ -124,7 +123,7 @@ object DockerWithML extends Logging {
       for (j <- 0 to Array2.length - 1)
         if (Array1(i) == Array2(j))
           counter = counter + 1
-    logInfo("Bad bins ranges from 0-" + params.badIn + " and good bins ranges from " + params.goodIn + "-10"  )      
+    logInfo("Bad bins ranges from 0-" + params.badIn + " and good bins ranges from " + params.goodIn + "-10")
     logInfo("Number of molecules matched are " + counter)
     logInfo("Percentage of same results is " + (counter / params.topN) * 100)
 
